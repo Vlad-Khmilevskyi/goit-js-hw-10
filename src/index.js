5265f41bf55aa07e03b42753a8780416e5c85780
ToFFeeV <khmiluvkiyv.ku@gmail.com>
2023-03-26 22:04:13 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,11 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries';
 
-import weatherCardTemplate from './templates/country-card.hbs';
-import countriesLists from './templates/country-list.hbs';
-
-
 const DEBOUNCE_DELAY = 300;
 
 const countriesList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
 const searchBox = document.querySelector('#search-box');
-const body = document.querySelector('body');
 
 searchBox.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
@@ -42,17 +37,38 @@ function onInputSearch(e) {
 
 function countriesListMarkup(result) {
     return result.map((({ name, flags }) => {
-        return  `<li class="country-item">
+        return `<li class="country-item">
                     <img src="${flags.svg}" alt="${name.official}" width="60" height="auto">
                     <span>${name.official}</span>
                 </li>`;
     })).join('');
 }
 
+function countryCardMarkup(result) {
+    return cardMarkup = result.map(({ flags, name, capital, population, languages }) => {
+        languages = Object.values(languages).join(", ");
+        return `<ul class="weather-info list">
+                    <li class="weather-info-item-map">
+                        <img src="${flags.svg}" alt="${name}" width="90" height="auto">
+                        <p class="temp">${name.official}</p>
+                    </li>
+                    <li class="weather-info-item">
+                        <p class="sunrise-time"><span class="bold">Capital:</span> ${capital}</p>
+                    </li>
+                    <li class="weather-info-item">
+                        <p class="sunset-time"><span class="bold">Population:</span> ${population}</p>
+                    </li>
+                    <li class="weather-info-item">
+                        <p class="clouds"><span class="bold">Languages:</span> ${languages}</p>
+                    </li>
+                </ul>`;
+    }).join('');
+}
+
 function renderCountries(result) {
     if (result.length === 1) {
     countriesList.innerHTML = '';
-    countryInfo.innerHTML = weatherCardTemplate(result);
+    countryInfo.innerHTML = countryCardMarkup(result);
     }
     if (result.length > 1 && result.length < 11) {
     countryInfo.innerHTML = '';
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,10 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
+const searchParams = new URLSearchParams({
+    fields: 'name,capital,population,flags,languages,'
+});
 
 export const fetchCountries = countryName => 
-    fetch(`${BASE_URL}${countryName}`)
+    fetch(`${BASE_URL}${countryName}?${searchParams}`)
     .then(response => {
         if (response.status === 404) {
             throw new Error(response.status);
